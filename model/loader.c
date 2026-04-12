5ae1e3c0a392353ac21e90c6b090b12baa0df259
Eduardo Oliveira <edudolivs@gmail.com>
2026-04-12 01:12:21 -0300

    style: change struct name to tensor

--- model/loader.c
+++ model/loader.c
@@ -3,37 +3,38 @@
 #include <stdint.h>
 #include <stdio.h>
 
-array_t *getArray() {
-  array_t *array = (array_t *)malloc(sizeof(array_t));
-  array->sizeDimentions = NULL;
-  array->data = NULL;
-  return array;
+tensor_t *getTensor() {
+  tensor_t *tensor = (tensor_t *)malloc(sizeof(tensor_t));
+  tensor->sizeDimentions = NULL;
+  tensor->data = NULL;
+  return tensor;
 }
 
-void freeArray(array_t *array) {
-  if (!array) {
+void freeTensor(tensor_t *tensor) {
+  if (!tensor) {
     return;
   }
-  if (array->sizeDimentions) {
-    free(array->sizeDimentions);
+  if (tensor->sizeDimentions) {
+    free(tensor->sizeDimentions);
   }
-  if (array->data) {
-    free(array->data);
+  if (tensor->data) {
+    free(tensor->data);
   }
-  array = NULL;
+  free(tensor);
+  tensor = NULL;
 }
 
-bool readNumDimention(array_t *array, FILE *file) {
+bool readNumDimention(tensor_t *tensor, FILE *file) {
   uint8_t magicNum[4];
 
   size_t bytesRead = fread(magicNum, 1, 4, file);
 
-  array->numDimentions = magicNum[3];
+  tensor->numDimentions = magicNum[3];
 
-  if (array->numDimentions == 3) {
-    array->category = IMAGE;
+  if (tensor->numDimentions == 3) {
+    tensor->category = IMAGE;
   } else {
-    array->category = LABEL;
+    tensor->category = LABEL;
   }
 
   return 0;
@@ -43,60 +44,60 @@ uint32_t swapBytes(uint32_t val) {
   return ((val << 24) | ((val << 8) & 0x00FF0000) | ((val >> 8) & 0x0000FF00) | (val >> 24));
 }
 
-bool readSizeDimention(array_t *array, FILE *file) {
-  array->sizeDimentions = (uint32_t *)malloc(array->numDimentions * sizeof(uint32_t));
+bool readSizeDimention(tensor_t *tensor, FILE *file) {
+  tensor->sizeDimentions = (uint32_t *)malloc(tensor->numDimentions * sizeof(uint32_t));
 
-  size_t bytesRead = fread(array->sizeDimentions, 4, array->numDimentions, file);
+  size_t bytesRead = fread(tensor->sizeDimentions, 4, tensor->numDimentions, file);
 
-  for (int i = 0; i < array->numDimentions; i++) {
-    array->sizeDimentions[i] = swapBytes(array->sizeDimentions[i]);
+  for (int i = 0; i < tensor->numDimentions; i++) {
+    tensor->sizeDimentions[i] = swapBytes(tensor->sizeDimentions[i]);
   }
 
   return 1;
 }
 
-bool readData(array_t *array, FILE *file) {
+bool readData(tensor_t *tensor, FILE *file) {
   int totalSize = 1;
 
-  for (int i = 0; i < array->numDimentions; i++) {
-    totalSize *= array->sizeDimentions[i];
+  for (int i = 0; i < tensor->numDimentions; i++) {
+    totalSize *= tensor->sizeDimentions[i];
   }
 
   uint8_t *buffer = (uint8_t *)malloc(totalSize * sizeof(uint8_t));
 
-  array->data = (float *)malloc(sizeof(float) * totalSize);
+  tensor->data = (float *)malloc(sizeof(float) * totalSize);
 
   size_t bytesRead = fread(buffer, 1, totalSize, file);
 
-  if (array->category == IMAGE) {
+  if (tensor->category == IMAGE) {
     for (int i = 0; i < totalSize; i++) {
-      array->data[i] = (float)buffer[i] / 255.0;
+      tensor->data[i] = (float)buffer[i] / 255.0;
     }
   } else {
     for (int i = 0; i < totalSize; i++) {
-      array->data[i] = (float)buffer[i];
+      tensor->data[i] = (float)buffer[i];
     }
   }
 
   return 0;
 }
 
-bool loadIdx(array_t *array, char *filePath) {
+bool loadIdx(tensor_t *tensor, char *filePath) {
   FILE *file = fopen(filePath, "rb");
 
-  readNumDimention(array, file);
+  readNumDimention(tensor, file);
 
-  readSizeDimention(array, file);
+  readSizeDimention(tensor, file);
 
-  readData(array, file);
+  readData(tensor, file);
 
   return 0;
 }
 
-void displayImage(array_t *array, int imageId) {
-  for (int line = 0; line < array->sizeDimentions[1]; line++) {
-    for (int column = 0; column < array->sizeDimentions[2]; column++) {
-      if (getPixelValue(array, imageId, line, column) < 0.5) {
+void displayImage(tensor_t *tensor, int imageId) {
+  for (int line = 0; line < tensor->sizeDimentions[1]; line++) {
+    for (int column = 0; column < tensor->sizeDimentions[2]; column++) {
+      if (getPixelValue(tensor, imageId, line, column) < 0.5) {
         printf(". ");
       } else {
         printf("0 ");
